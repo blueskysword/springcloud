dd001032aaa3943841820c107519a0913fe83774
blueskysword <1372101711@qq.com>
2020-06-10 00:07:01 +0800

    eureka注册集群及ribbon负载
    Signed-off-by: blueskysword <1372101711@qq.com>

--- demo/eureka-client-producer2/src/main/java/com/example/eurekaclientproducer/controller/TestController.java
+++ demo/eureka-client-producer2/src/main/java/com/example/eurekaclientproducer/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * User: lr
@@ -28,6 +29,7 @@ public class TestController {
 
     @RequestMapping(value = "/demo2",method = RequestMethod.GET)
     public String test2(){
+        System.out.println("#########producer2############"+new Date());
         InstanceInfo info =  eurekaClient.getNextServerFromEureka("eureka-producer",false);
         return info.getHomePageUrl();
     }
--- demo/eureka-consumer/src/main/java/com/example/eurekaconsumer/controller/UserController.java
+++ demo/eureka-consumer/src/main/java/com/example/eurekaconsumer/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.eurekaconsumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * User: lr
@@ -15,16 +18,23 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/consumer")
 public class UserController {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
-    @Autowired
+    @Resource
     private RestTemplate restTemplate;
 
     @RequestMapping("/demo")
     public String consumerdemo() {
         return this.restTemplate.getForObject("http://localhost:9000/producer/demo",String.class);
+
+    }
+
+    @RequestMapping("/ribbon")
+    public String consumerdemo2() {
+        return this.restTemplate.getForObject("http://eureka-producer/producer/demo2",String.class);
 
     }
 
